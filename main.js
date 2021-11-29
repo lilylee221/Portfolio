9df3e6e147fcf0d09dd1d63f0d9c2eb460b762ef
lilylee221 <lil.lee0221@gmail.com>
2021-11-29 10:35:05 +0100

    JS: Home contact btn

--- main.js
+++ main.js
@@ -10,3 +10,35 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// Scroll to the sction when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+//scrollIntoView function
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'nearest',
+  });
+}
+
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+//contact me btn handling
+
+const contactBtn = document.querySelector('.home__contact');
+
+contactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
