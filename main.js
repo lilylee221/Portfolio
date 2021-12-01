1a263e1fead4ae4972569181c61fd96b65f7b88c
lilylee221 <lil.lee0221@gmail.com>
2021-12-01 12:48:10 +0100

    JS: Navbar item active upon scrolling

--- main.js
+++ main.js
@@ -26,16 +26,6 @@ navbarToggleBtn.addEventListener('click', () => {
 
 const navbarMenu = document.querySelector('.navbar__menu');
 
-//scrollIntoView function
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-    inline: 'nearest',
-  });
-}
-
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
@@ -66,7 +56,6 @@ contactBtn.addEventListener('click', () => {
 //Make home slowly fade to transparent as the window scrolls down
 const homeSection = document.querySelector('.home__container');
 const homeHeight = homeSection.getBoundingClientRect().height;
-console.log(homeHeight);
 document.addEventListener('scroll', () => {
   homeSection.style.opacity = 1 - window.scrollY / homeHeight;
 });
@@ -116,4 +105,81 @@ projectBtnContainer.addEventListener('click', (event) => {
     });
     projectContainer.classList.remove('anim-out');
   }, 300);
+});
+
+// intersectionobserver to active navbar__menu when scrolling(wheel)
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+
+//  1.
+//Ids - string array
+const sectionIds = ['#home', '#about', '#skills', '#projects', '#contact'];
+// all sections in an array
+const sections = sectionIds.map((id) => document.querySelector(id));
+//all navitems
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+// 2. 현재 선택된 메뉴 인덱스와 메뉴 요소를 변수에 저장 , 새 메뉴 선택시 이전 클래스 리무브, 새 할당, 액티브 지정
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+//scrollIntoView function
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'nearest',
+  });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: '0.3',
+};
+
+// intersectionObserver callback -
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      //when section not entering & ratio is more than 0 -> only to call 'home'??
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        //section scrolled down
+        selectedNavIndex = index + 1;
+      } else {
+        // scrolled up
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+// with the above index info, wheel = 0 -> home /
+// window.scrollY + window.innerHeight === document.body.clientHeight => the last item
+window.addEventListener('wheel', () => {
+  if (window.scrolly === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    window.scrollY + window.innerHeight ===
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]); // else
 });
